fed6e82fcb2e105cc24782cfa2624fe46e382b7b
Evgeny Golyshev <eugulixes@gmail.com>
2016-11-19 04:03:29 +0300

    Bump client and server version number to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='shirow',
-      version='0.1',
+      version='0.2',
       description='Shirow is an implementation of a distinctive concept of a ' \
                   'remote procedure call.',
       url='https://bitbucket.org/eugulixes/shirow',
