269b1e97b05e0569c9b65b2dab71133ccd0591d4
Evgeny Golyshev <eugulixes@gmail.com>
2020-04-19 19:26:24 +0300

    Make shirow/test/runtests.py meet pylint requirements

--- shirow/test/runtests.py
+++ shirow/test/runtests.py
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""This module contains the Shirow tests. """
+
 import datetime
 import logging
 import os
 import pty
-import sys
 
 import jwt
 from tornado import gen
@@ -29,7 +30,6 @@ from tornado.testing import AsyncHTTPTestCase, gen_test
 from tornado.web import Application
 from tornado.websocket import websocket_connect
 
-from shirow import util
 from shirow.server import RPCServer, MOCK_TOKEN, TOKEN_PATTERN, remote
 
 TOKEN_ALGORITHM_ENCODING = 'HS256'
@@ -47,10 +47,23 @@ EXPIRED_ENCODED_TOKEN = jwt.encode(
 ).decode('utf8')
 
 
-class MockRPCServer(RPCServer):
+def prepare_payload(procedure_name, parameters_list, marker):
+    """Prepares a valid payload for use in the test cases. """
+
+    data = {
+        'function_name': procedure_name,
+        'parameters_list': parameters_list,
+        'marker': marker
+    }
+    return json_encode(data)
+
+
+class MockRPCServer(RPCServer):  # pylint: disable=abstract-method
+    """An RPC server based on Shirow used for the testing purposes. """
+
     def initialize(self, close_future, compression_options=None):
-        self.close_future = close_future
-        self.compression_options = compression_options
+        self.close_future = close_future  # pylint: disable=attribute-defined-outside-init
+        self.compression_options = compression_options  # pylint: disable=attribute-defined-outside-init
 
     def get_compression_options(self):
         return self.compression_options
@@ -59,31 +72,31 @@ class MockRPCServer(RPCServer):
         self.close_future.set_result((self.close_code, self.close_reason))
 
     @remote
-    async def add(self, request, a, b):
-        return a + b
+    async def add(self, _request, op_a, op_b):
+        return op_a + op_b
 
     @remote
-    async def div_by_zero(self, request):
-        1 / 0
+    async def div_by_zero(self, _request):
+        1 / 0  # pylint: disable=pointless-statement
 
     @remote
     async def echo_via_ret_method(self, request, message):
         request.ret(message)
 
     @remote
-    async def echo_via_return_statement(self, request, message):
+    async def echo_via_return_statement(self, _request, message):
         return message
 
     @remote
-    async def read_from_fd(self, request, fd):
-        def handler(*args, **kwargs):
-            res = os.read(fd, 65536)
+    async def read_from_fd(self, request, master_fd):
+        def handler(*_args, **_kwargs):
+            res = os.read(master_fd, 65536)
             if res == b'qux\r\n':
                 request.ret(res.decode('utf8'))
 
             request.ret_and_continue(res.decode('utf8'))
 
-        self.io_loop.add_handler(fd, handler, self.io_loop.READ)
+        self.io_loop.add_handler(master_fd, handler, self.io_loop.READ)
 
     @remote
     async def return_more_than_one_value(self, request):
@@ -92,11 +105,13 @@ class MockRPCServer(RPCServer):
         request.ret_and_continue('eggs')
 
     @remote
-    async def say_hello(self, request, name='Shirow'):
+    async def say_hello(self, _request, name='Shirow'):
         return f'Hello {name}!'
 
 
-class WebSocketBaseTestCase(AsyncHTTPTestCase):
+class WebSocketBaseTestCase(AsyncHTTPTestCase):  # pylint: disable=abstract-method
+    """A test case that starts up a WebSocket server. """
+
     def setUp(self):
         super(WebSocketBaseTestCase, self).setUp()
         IOLoop.configure('tornado.platform.asyncio.AsyncIOLoop')
@@ -104,22 +119,24 @@ class WebSocketBaseTestCase(AsyncHTTPTestCase):
 
     @gen.coroutine
     def ws_connect(self, path, compression_options=None):
-        ws = yield websocket_connect(f'ws://127.0.0.1:{self.get_http_port()}{path}',
-                                     compression_options=compression_options)
-        return ws
+        ws_conn = yield websocket_connect(f'ws://127.0.0.1:{self.get_http_port()}{path}',
+                                          compression_options=compression_options)
+        return ws_conn
 
     @gen.coroutine
-    def close(self, ws):
+    def close(self, ws_conn):
         """Close a websocket connection and wait for the server side.
 
         If we don't wait here, there are sometimes leak warnings in the
         tests.
         """
-        ws.close()
-        yield self.close_future
+        ws_conn.close()
+        yield self.close_future  # pylint: disable=no-member
 
 
 class RPCServerTest(WebSocketBaseTestCase):
+    """Tests various scenarios for an RPC server based on Shirow. """
+
     def get_app(self):
         self.close_future = Future()
         options.token_algorithm = TOKEN_ALGORITHM_ENCODING
@@ -130,14 +147,6 @@ class RPCServerTest(WebSocketBaseTestCase):
             ('/rpc/token/' + TOKEN_PATTERN, MockRPCServer,
              dict(close_future=self.close_future)),
         ])
-
-    def prepare_payload(self, procedure_name, parameters_list, marker):
-        data = {
-            'function_name': procedure_name,
-            'parameters_list': parameters_list,
-            'marker': marker
-        }
-        return json_encode(data)
 
     def test_tokenless_request(self):
         response = self.fetch('/rpc')
@@ -172,136 +181,135 @@ class RPCServerTest(WebSocketBaseTestCase):
 
     @gen_test
     def test_using_ret_method_to_return_value(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
-        payload = self.prepare_payload('echo_via_ret_method', ['Hello!'], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        payload = prepare_payload('echo_via_ret_method', ['Hello!'], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'Hello!',
             'marker': 1,
             'eod': 1,
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_using_return_statement_to_return_value(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
-        payload = \
-            self.prepare_payload('echo_via_return_statement', ['Hello!'], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        payload = prepare_payload('echo_via_return_statement', ['Hello!'], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'Hello!',
             'marker': 1,
             'eod': 1,
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_calling_non_existent_function(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
-        payload = self.prepare_payload('non_existent_function', [], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        payload = prepare_payload('non_existent_function', [], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'error': 'the non_existent_function function is undefined',
             'marker': 1
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_mismatching_parameters(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
         # less than is required
-        payload = self.prepare_payload('add', [1], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        payload = prepare_payload('add', [1], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'error': 'number of arguments mismatch in the add function call',
             'marker': 1
         })
         # more than is required
-        payload = self.prepare_payload('add', [1, 3, 5], 2)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        payload = prepare_payload('add', [1, 3, 5], 2)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'error': 'number of arguments mismatch in the add function call',
             'marker': 2
         })
 
-        payload = self.prepare_payload('add', [1, 3], 3)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        payload = prepare_payload('add', [1, 3], 3)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 4,
             'marker': 3,
             'eod': 1,
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_handling_errors_in_remote_procedures(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
-        payload = self.prepare_payload('div_by_zero', [], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        payload = prepare_payload('div_by_zero', [], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'error': 'an error occurred while executing the function div_by_zero',
             'marker': 1
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_default_parameters(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
-        payload = self.prepare_payload('say_hello', [], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        payload = prepare_payload('say_hello', [], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'Hello Shirow!',
             'marker': 1,
             'eod': 1,
         })
-        payload = self.prepare_payload('say_hello', ['Norris'], 2)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        payload = prepare_payload('say_hello', ['Norris'], 2)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'Hello Norris!',
             'marker': 2,
             'eod': 1,
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_returning_more_than_one_value(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
-        payload = self.prepare_payload('return_more_than_one_value', [], 1)
-        ws.write_message(payload)
-        response = yield ws.read_message()
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        payload = prepare_payload('return_more_than_one_value', [], 1)
+        ws_conn.write_message(payload)
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'spam',
             'marker': 1,
             'eod': 0,
         })
-        response = yield ws.read_message()
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'ham',
             'marker': 1,
             'eod': 0,
         })
-        response = yield ws.read_message()
+        response = yield ws_conn.read_message()
         self.assertEqual(json_decode(response), {
             'result': 'eggs',
             'marker': 1,
             'eod': 0,
         })
-        yield self.close(ws)
+        yield self.close(ws_conn)
 
     @gen_test
     def test_reading_from_fd(self):
-        ws = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
+        ws_conn = yield self.ws_connect(f'/rpc/token/{ENCODED_TOKEN}')
 
-        pid, fd = pty.fork()
+        pid, master_fd = pty.fork()
         if pid == 0:  # child
             script = (
                 'from time import sleep\n'
@@ -310,43 +318,46 @@ class RPCServerTest(WebSocketBaseTestCase):
             command_line = ['python3', '-c', script]
             os.execvp(command_line[0], command_line)
         else:  # parent
-            payload = self.prepare_payload('read_from_fd', [fd], 1)
-            ws.write_message(payload)
-            response = yield ws.read_message()
+            payload = prepare_payload('read_from_fd', [master_fd], 1)
+            ws_conn.write_message(payload)
+            response = yield ws_conn.read_message()
             self.assertEqual(json_decode(response), {
                 'result': 'foo\r\n',
                 'marker': 1,
                 'eod': 0,
             })
-            response = yield ws.read_message()
+            response = yield ws_conn.read_message()
             self.assertEqual(json_decode(response), {
                 'result': 'bar\r\n',
                 'marker': 1,
                 'eod': 0,
             })
-            response = yield ws.read_message()
+            response = yield ws_conn.read_message()
             self.assertEqual(json_decode(response), {
                 'result': 'baz\r\n',
                 'eod': 0,
                 'marker': 1
             })
-            response = yield ws.read_message()
+            response = yield ws_conn.read_message()
             self.assertEqual(json_decode(response), {
                 'result': 'qux\r\n',
                 'marker': 1,
                 'eod': 1,
             })
 
-            yield self.close(ws)
+            yield self.close(ws_conn)
 
 
 def main():
+    """The main entry point. """
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     handler = logging.FileHandler('shirow_test.log')
     logger.addHandler(handler)
 
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
