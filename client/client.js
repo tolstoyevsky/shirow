024d9df4260610936dae30cb2a1a63da4a39e2fc
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-04-20 22:48:05 +0300

    Add Denis Gavrilyuk copyright to client/client.js

--- client/client.js
+++ client/client.js
@@ -2,6 +2,7 @@
  * Copyright 2016 Maxim Karpinskiy <karpinski20@gmail.com>
  * Copyright 2016 Denis Mosolov <denismosolov@gmail.com>
  * Copyright 2016 Evgeny Golyshev <eugulixes@gmail.com>
+ * Copyright 2020 Denis Gavrilyuk <karpa4o4@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
